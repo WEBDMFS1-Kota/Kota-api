6d949c8cad6b5246d29c19b5c1b7ecf2b377e97c
Adrian-Paul Carrières <adrian.paul.carrieres@gmail.com>
2022-05-31 17:25:41 +0200

    eslint working

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,17 @@
-import fastify from 'fastify'
+import fastify from 'fastify';
 
-const server = fastify()
+const server = fastify();
 
 server.get('/ping', async (request, reply) => {
-  return 'pong!!\n'
-})
+  console.log(request);
+  // console.log(reply);
+  return 'pong!!\n';
+});
 
 server.listen(8080, (err, address) => {
   if (err) {
-    console.error(err)
-    process.exit(1)
+    console.error(err);
+    process.exit(1);
   }
-  console.log(`Server listening at ${address}`)
-})
+  console.log(`Server listening at ${address}`);
+});
