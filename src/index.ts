99cb4c66f8cb15565c072711e1cdd5a410c7c8d1
AliceH <lotus.b78@gmail.com>
2022-06-08 00:28:57 +0200

    Modification suite remarques + class Error

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,10 @@
 import fastify from 'fastify';
-<<<<<<< HEAD
 import projectRoutes from './routes/project/projectRoute';
 
 const server = fastify({ logger: true });
+
 server.register(projectRoutes);
-=======
 
-// Routes
-import projetRoutes from './routes/projet/projetRoute';
-import userRoutes from './routes/user/userRoute';
-
-const dotenv = require('dotenv');
-
-dotenv.config();
-
-const server = fastify({ logger: true });
-
-// Registering routes in fastify
-server.register(projetRoutes);
-server.register(userRoutes);
->>>>>>> 30ec90ea4ea0e0ceaf5550fd336bb1f101bcc6a4
-
-// Configuring and starting the server
 const port = process.env.PORT || 8080;
 const host = process.env.HOST || '0.0.0.0';
 
