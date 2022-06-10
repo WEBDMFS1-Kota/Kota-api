abdc457a8d85df22183cdb1ae45a1a02b384a42e
Paul <pld.pro@hotmail.com>
2022-06-10 11:18:40 +0200

    correction merge user routes reimported

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import fastify from 'fastify';
 import projectRoutes from './routes/project/projectRoute';
+import userRoutes from './routes/user/userRoute';
 
 const server = fastify({ logger: true });
 
 server.register(projectRoutes);
+server.register(userRoutes);
 
 const port = process.env.PORT || 8080;
 const host = process.env.HOST || '0.0.0.0';
