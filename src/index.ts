c92ba91b274d85caf9de6863333b9b45f1387911
Paul <pld.pro@hotmail.com>
2022-06-14 18:07:30 +0200

    woppsi host

--- src/index.ts
+++ src/index.ts
@@ -17,8 +17,8 @@ server.register(tagRoutes);
 server.register(userTagsRoutes);
 
 const port = Number(process.env.PORT || 8080);
-// const host = process.env.HOST || '0.0.0.0';
-server.listen({ port }, (err, address) => {
+const host = process.env.HOST || '0.0.0.0';
+server.listen({ port, host }, (err, address) => {
   if (err) {
     console.error(err);
     process.exit(1);
