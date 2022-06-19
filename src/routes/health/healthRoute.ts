6bd5dcf4d3e63807f643e1ae5c3de3e44d47411d
Paul <pld.pro@hotmail.com>
2022-06-19 17:38:50 +0200

    Health route Fixes #27 - return json

--- src/routes/health/healthRoute.ts
+++ src/routes/health/healthRoute.ts
@@ -3,10 +3,24 @@ import { health } from '../../services/health/healthService';
 const healthRoute = (server: any, opts: any, done: () => void) => {
   server.get('/health', async () => {
     try {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
       const healthStatus = await health();
-      return `Returned the number of available tags as a database health status report : ${healthStatus}.`;
+      return {
+        response: {
+          200: {
+            description: 'OK',
+          },
+        },
+      };
     } catch (error) {
-      return error;
+      return {
+        response: {
+          500: {
+            description: 'Database error',
+            message: error,
+          },
+        },
+      };
     }
   });
   done();
