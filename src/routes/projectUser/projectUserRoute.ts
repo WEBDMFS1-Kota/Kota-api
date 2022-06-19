3caa9d1ddee3d2b2ffc2457908f53117cc6bb821
Paul <pld.pro@hotmail.com>
2022-06-19 22:36:52 +0200

    Add project's creator id to the return statement Fixes #37 - ownerRoute

--- src/routes/projectUser/projectUserRoute.ts
+++ src/routes/projectUser/projectUserRoute.ts
@@ -1,17 +1,29 @@
 import { getProjectById } from '../../services/project/projectService';
-import { getProjectsUser } from '../../services/projectUser/projectUserService';
+import { getProjectsUserByUser, getProjectsUserByProject } from '../../services/projectUser/projectUserService';
+import { getUser } from '../../services/user/userService';
 
 const ProjectUserRoutes = (server: any, opts: any, done: () => void) => {
   server.get('/users/:userId/projects', async (request: any) => {
     const { params, query } = request;
     try {
       const projectsUser: any[] = [];
-      const projectsUserRelations = await getProjectsUser(params.userId, query);
+      const projectsUserRelations = await getProjectsUserByUser(params.userId, query);
       await Promise.all(projectsUserRelations.map(async (relation) => {
         const project = await getProjectById(Number(relation.projectId));
         projectsUser.push(project);
       }));
       return projectsUser;
+    } catch (error) {
+      return error;
+    }
+  });
+
+  server.get('/users/:projectId/projectCreator', async (request: any) => {
+    const { params, query } = request;
+    try {
+      const projectsUserRelations = (await getProjectsUserByProject(params.projectId, query))[0];
+      const ownerInfos = (await getUser(projectsUserRelations))[0];
+      return ownerInfos;
     } catch (error) {
       return error;
     }
--- src/services/user/userService.ts
+++ src/services/user/userService.ts
@@ -23,11 +23,22 @@ async function getUser(query: any) {
       pseudo: query.pseudo,
       email: query.email,
     },
+    select: {
+      id: true,
+      pseudo: true,
+      avatar: true,
+      firstname: true,
+      lastname: true,
+      password: false,
+      email: true,
+      birthDate: true,
+      githubProfileURL: true,
+    },
   });
   return user;
 }
 
-async function updateUser(userCheckedId:any, body: any) {
+async function updateUser(userCheckedId: any, body: any) {
   const birthdate = new Date(body.birthDate);
   return prisma.users.update({
     where: {
@@ -46,7 +57,7 @@ async function updateUser(userCheckedId:any, body: any) {
   });
 }
 
-async function deleteUser(query:any) {
+async function deleteUser(query: any) {
   return prisma.users.delete({
     where: {
       id: Number(query.id),
