93b4622413279981fce0faba90256967677859db
Elouan MAILLY <leloupandco@gmail.com>
2022-06-20 00:29:14 +0200

    feat: add complete documentation for tags routes
    Signed-off-by: Elouan MAILLY <leloupandco@gmail.com>

--- src/routes/tag/tagRoute.ts
+++ src/routes/tag/tagRoute.ts
@@ -1,16 +1,20 @@
 import {
   getTagsByName,
 } from '../../services/tag/tagService';
+import { getTagsSchema } from '../../schema/tagsSchema';
 
 const tagRoutes = (server: any, opts: any, done: () => void) => {
-  server.get('/tags', async (request: any) => {
-    const { query } = request;
-    try {
-      const tags = await getTagsByName(query);
-      return tags;
-    } catch (error) {
-      return error;
-    }
+  server.get('/tags', {
+    schema: getTagsSchema,
+    handler: async (request: any) => {
+      const { query } = request;
+      try {
+        const tags = await getTagsByName(query);
+        return tags;
+      } catch (error) {
+        return error;
+      }
+    },
   });
 
   done();
