5dd68fde259496b363fc0a6cc76f1b3115c49f10
Paul <pld.pro@hotmail.com>
2022-06-20 19:48:42 +0200

    rename

--- src/routes/forgottenPassword/forgottenPasswordRoute.ts
+++ src/routes/forgottenPassword/forgottenPasswordRoute.ts
@@ -1,13 +1,12 @@
-import { sendEmail } from '../../services/resetPassword/resetPasswordService';
-import { getUsers } from '../../services/user/userService';
+import { sendEmail } from '../../services/forgottenPassword/forgottenPasswordService';
 
 const forgottenPasswordRoute = (server: any, opts: any, done: () => void) => {
   server.get('/resetPassword', async (request: any) => {
     try {
       const { query } = request;
-      const userToSend = (await getUsers(query))[0];
       // eslint-disable-next-line @typescript-eslint/no-unused-vars
-      const sendResetmail = await sendEmail(userToSend, 'lol');
+      const options = { emailID: 'test' };
+      const sendResetmail = await sendEmail(query, options);
       return sendResetmail;
     } catch (error) {
       return error;
