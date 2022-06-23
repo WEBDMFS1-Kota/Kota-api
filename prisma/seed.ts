84fba14098e9d325ca50798910b591c0190cfd40
Adrian-Paul Carrières <adrian-paul.carrieres@myriadgroup.com>
2022-06-23 10:01:29 +0200

    seed

---
+++ prisma/seed.ts
@@ -0,0 +1,25 @@
+import { PrismaClient } from '@prisma/client'
+import tags from './tags.json';
+
+console.table(tags);
+
+const prisma = new PrismaClient();
+
+async function main() {
+  console.log('=== Begin Seed ===');
+
+  const result = await prisma.tags.createMany({
+    data: tags,
+    skipDuplicates: true,
+  });
+
+  console.log(`Inserted ${result.count} records`);
+}
+
+main()
+  .catch((e) => {
+    throw e;
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
